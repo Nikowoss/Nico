06cc59dec39796b09ddabcf7a4bbfc4d366e7c6c
Gabylina <gab.sandoval@duocuc.cl>
2023-06-04 16:42:16 -0400

    Botón pagar

--- Pagina_vin/js/FormularioSolicitudes.js
+++ Pagina_vin/js/FormularioSolicitudes.js
@@ -81,39 +81,67 @@ function a() {
     });
 }
 
+function pagar_presupuesto() {
+    swal({
+        title: "Estamos redireccionando al medio de pago",
+        icon: "info",
+    })
+    .then(presupuestoPagado => {
+        if (presupuestoPagado) {
+            swal({
+                title: "Pago recepcionado con éxito", 
+                icon: "success",
+            });
+        }
+    });
+    var boton = document.getElementById('boton')
+    boton.disabled=true;
+}
+
+
+
 function Modificar(){
-    var user = document.getElementById('username');
-    var tele = document.getElementById('tele');
-    var Direccion = document.getElementById('Direccion');
     var rut = document.getElementById('rut');
+    var nombre = document.getElementById('nombre');
+    var apellido = document.getElementById('apellido');
+    var correo = document.getElementById('correo');
+    var tele = document.getElementById('tele');
+    var Direccion = document.getElementById('direccion');
     var btn = document.getElementById('boton')
     var btn2 = document.getElementById('boton2')
 
-    user.disabled = false;
-    tele.disabled = false;
-    Direccion.disabled = false;
     rut.disabled = false;
+    nombre.disabled = false;
+    apellido.disabled = false;
+    correo.disabled = false;
+    tele.disabled = false;
+    direccion.disabled = false;
     btn.disabled = true;
     btn2.disabled = false;
 
 }
 
 function Guardar(){
-    var user = document.getElementById('username');
-    var tele = document.getElementById('tele');
-    var Direccion = document.getElementById('Direccion');
     var rut = document.getElementById('rut');
+    var nombre = document.getElementById('nombre');
+    var apellido = document.getElementById('apellido');
+    var correo = document.getElementById('correo');
+    var tele = document.getElementById('tele');
+    var Direccion = document.getElementById('direccion');
     var btn = document.getElementById('boton')
     var btn2 = document.getElementById('boton2')
 
-    user.disabled = true;
-    tele.disabled = true;
-    Direccion.disabled = true;
-    rut.disabled = true;
+    rut.disabled = false;
+    nombre.disabled = false;
+    apellido.disabled = false;
+    correo.disabled = false;
+    tele.disabled = false;
+    direccion.disabled = false;
     btn.disabled = false;
     btn2.disabled = true;
 
-    swal("Solicitud enviada con exito", {
+    swal({
+        title:"Datos guardados con éxito", 
         icon: "success",
     });
 }
